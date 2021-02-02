20fa2b86d49dcea234772c10ae98263c8871bb10
kkiryulab <kkiryu@dai-ichi-lab.com>
2021-02-02 09:03:23 +0000

    vue-router

---
+++ spa_demo/src/App_copy.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p>
+            {{msg}}
+        </p>
+        <input type="text" v-model="msg">
+        <button @click="clear()">Clear</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            msg: 'Hello World!'
+        }
+    },
+    methods: {
+        clear(){
+            this.msg = ''
+        }
+    }
+}
+</script>
---
+++ spa_demo/src/components/page2.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <p>
+            ここはページ2です。
+        </p>
+        <router-link to="/">Go to page1</router-link>
+    </div>
+</template>
